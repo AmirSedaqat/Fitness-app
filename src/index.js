608c85d63f29d17e655cdd9fda8c8444fe9a1135
Amir Sedaqat <amirsedaghat.dev@gmail.com>
2022-07-22 04:37:22 +0430

    Project structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
