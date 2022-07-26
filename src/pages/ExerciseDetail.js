d2ff46c54a829fd8fbb1e86b90ea2f8fbb376f88
Amir Sedaqat <amirsedaghat.dev@gmail.com>
2022-07-26 04:52:38 +0430

    The ExerciseDetail.js component structure is created

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+// Router-dom
+import { useParams } from "react-router-dom";
+
+// MUI
+import { Box } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+// Import components
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
-    return (
-        <div>
+    const [exerciseDetail, setExerciseDetail] = useState({});
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            const exercisesDbUrl = "https://exercisedb.p.rapidapi.com";
+            const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com";
+            const exercisesDetailData = await fetchData(`${exercisesDbUrl}/exercises/exercise/${id}`, exerciseOptions);
             
-        </div>
+            setExerciseDetail(exercisesDetailData);
+        };
+
+        fetchExercisesData();
+    }, [id]);
+
+    return (
+        <Box>
+            <Detail exerciseDetail={exerciseDetail} />
+            <ExerciseVideos />
+            <SimilarExercises />
+        </Box>
     );
 };
 
